f6293e157acad61caf13a27dc72c4e718cfbe691
tellaw <tellaw@MacBook-Pro-de-Eric-3.local>
2014-07-20 21:53:04 +0200

    update model

---
+++ src/Tellaw/LeadsFactoryBundle/Entity/Form.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Tellaw\LeadsFactoryBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ *
+ * Tellaw\LeadsFactoryBundle\Entity\Form
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Form {
+
+	/**
+	 * @var integer $id
+	 *
+	 * @ORM\Column(name="id", type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+    protected $id;
+
+    /**
+     * @var string $name
+     * @ORM\Column(name="name", type="string", nullable=true)
+     */
+    protected $name;
+
+    /**
+     * @var longtext $description
+     * @ORM\Column (name="description", type="text", nullable=true)
+     */
+    protected $description;
+    protected $source;
+
+    /**
+     * @param \Tellaw\LeadsFactoryBundle\Entity\longtext $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return \Tellaw\LeadsFactoryBundle\Entity\longtext
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $script
+     */
+    public function setScript($script)
+    {
+        $this->script = $script;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getScript()
+    {
+        return $this->script;
+    }
+
+    /**
+     * @param mixed $source
+     */
+    public function setSource($source)
+    {
+        $this->source = $source;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSource()
+    {
+        return $this->source;
+    }
+
+    /**
+     * @param mixed $style
+     */
+    public function setStyle($style)
+    {
+        $this->style = $style;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStyle()
+    {
+        return $this->style;
+    }
+    protected $script;
+    protected $style;
+
+}
