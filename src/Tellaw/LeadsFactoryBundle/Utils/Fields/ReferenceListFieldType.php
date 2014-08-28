3e9fb4db51911bcd3fbf93a60190d9e4362880bf
Eric Wallet <eric.wallet@yahoo.fr>
2014-08-28 10:06:10 +0200

    update

--- src/Tellaw/LeadsFactoryBundle/Utils/Fields/ReferenceListFieldType.php
+++ src/Tellaw/LeadsFactoryBundle/Utils/Fields/ReferenceListFieldType.php
@@ -7,7 +7,7 @@ use Tellaw\LeadsFactoryBundle\Utils\Fields\AbstractFieldType;
  * Field of type ReferenceList will be identified by the type <b>reference-list</b> and will be used to display content of a reference list.
  *  *
  * <b>selection</b> : "multiple" or "single" will switch from Select field to checkbox field<br/>
- * <b>display</b> : "options" will force display of options instead of default selection
+ * <b>display</b> : "options" will force display of options instead of default selection (select)
  *
  * Exemple : &lt;field type="reference-list" selection="single" display="options"/>
  *
