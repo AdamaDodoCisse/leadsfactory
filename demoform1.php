3cb1d484d3a40882815266234a3be53aed10ba8f
tellaw <ewalletfr@gmail.com>
2014-09-01 16:03:53 +0200

    update js lib

--- demoform1.php
+++ demoform1.php
@@ -1,13 +1,16 @@
 <html>
 <head>
-    <script type="text/javascript" src="http://local.dev/leadsfactory/web/app_dev.php/client/form/js/1" ></script>
+    <script type="text/javascript" src="http://local.dev/leadsfactory/web/app_dev.php/client/form/js/3" ></script>
 </head>
 <body>
 
 <h1>Affichage formulaire</h1>
 <script>
-    displayFrm();
-    setLfLastname ('tellaw');
+
+    leadsfactory.render();
+
+    leadsfactory.setNom ('Le nom');
+    leadsfactory.setPrenom ('Le prénom');
 
 </script>
 </body>
