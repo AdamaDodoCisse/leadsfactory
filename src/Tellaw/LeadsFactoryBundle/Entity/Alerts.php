67a3792fc2c8f6fb02dd5f6e09fefcbd481107dd
tellaw <ewalletfr@gmail.com>
2014-09-09 11:16:52 +0200

    added alert object

---
+++ src/Tellaw/LeadsFactoryBundle/Entity/Alerts.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace Tellaw\LeadsFactoryBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="export_jobs")
+ */
+class Alert {
+
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * 0 : Form | 1 : Type
+     * @ORM\Column(type="integer", nullable=false, name="type")
+     */
+    protected $type;
+
+    /**
+     * 0 : - | 1 : Warning | 2 : Error
+     * @ORM\Column(type="integer", nullable=false, name="state")
+     */
+    protected $state;
+
+    /**
+     * 0 : max | 1 : min | 2 : Delta
+     * @ORM\Column(type="integer", nullable=false, name="cause")
+     */
+    protected $cause;
+
+    /**
+     * Id of the target object
+     * @ORM\Column(type="integer", nullable=false, name="source")
+     */
+    protected $source;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=false, name="created_at")
+     */
+    protected $created_at;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    protected $log;
+
+    /**
+     * @param mixed $cause
+     */
+    public function setCause($cause)
+    {
+        $this->cause = $cause;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCause()
+    {
+        return $this->cause;
+    }
+
+    /**
+     * @param mixed $created_at
+     */
+    public function setCreatedAt($created_at)
+    {
+        $this->created_at = $created_at;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $log
+     */
+    public function setLog($log)
+    {
+        $this->log = $log;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLog()
+    {
+        return $this->log;
+    }
+
+    /**
+     * @param mixed $source
+     */
+    public function setSource($source)
+    {
+        $this->source = $source;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSource()
+    {
+        return $this->source;
+    }
+
+    /**
+     * @param mixed $state
+     */
+    public function setState($state)
+    {
+        $this->state = $state;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * @param mixed $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+}
