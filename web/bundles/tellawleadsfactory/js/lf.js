ccd6fe68147e57b2ec3b9be8a0b62a756df05e0c
nicolas <nicrobelin@gmail.com>
2014-10-08 11:03:50 +0200

    Listes à n niveaux avec input text

--- web/bundles/tellawleadsfactory/js/lf.js
+++ web/bundles/tellawleadsfactory/js/lf.js
@@ -20,21 +20,41 @@ $.fn.lfList = function(options){
         var defaultLbl = $(this).data('default');
         if(parent){
             var parent_id = 'lffield\\['+parent+'\\]';
-            $('#'+parent_id).change(function(){
-                updateChildOptions(parent, $(this).val(), child, defaultLbl);
-            })
+            var parent_list_code = $('#'+parent_id).data('list');
+
+            if($('#'+parent_id).is('input')){
+                $('#'+parent_id).on('input', function(){
+                    var validation = $(this).validationEngine('validate');
+                    //$(this).validationEngine('hideAll');
+                    if(!validation){
+                        updateChildOptions(parent_list_code, $(this).val(), child, defaultLbl);
+                    }
+                })
+
+            }else{
+
+                $('#'+parent_id).change(function(){
+                    updateChildOptions(parent_list_code, $(this).val(), child, defaultLbl);
+                })
+            }
         }
     })
 
-    updateChildOptions = function(parent, parentValue, child, defaultLbl){
-        $.get(settings.ajax_url, {'parent_code': parent, 'parent_value': parentValue, 'default': defaultLbl}, function(data, textStatus){
+    updateChildOptions = function(parent_list_code, parentValue, child, defaultLbl){
+        $.get(settings.ajax_url, {'parent_code': parent_list_code, 'parent_value': parentValue, 'default': defaultLbl}, function(data, textStatus){
+
+            /** Mise à jour de l'enfant **/
             child.html(data);
-            /** Mise à jour de l'enfant de l'enfant si besoin **/
+
+            /** Mise à jour de l'enfant de l'enfant, si besoin **/
             var child_id = child.attr('id');
             var child_code = child_id.replace(/lffield\[(\w+)\]/, "$1");
             var childChild = $('select[data-parent='+child_code+']');
+            var child_list_code = child.data('list');
+
+            /** Rebelote **/
             if(childChild.length){
-                updateChildOptions(child_code, child.val(), childChild, childChild.data('default'));
+                updateChildOptions(child_list_code, child.val(), childChild, childChild.data('default'));
             }
         });
     }
