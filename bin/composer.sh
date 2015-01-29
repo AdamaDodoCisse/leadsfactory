095d4337ff5b8b9daf12d04c7338c193007a8421
olombard <olombard@weka.fr>
2015-01-29 11:25:49 +0100

    Script bin/composer.sh plus générique

--- bin/composer.sh
+++ bin/composer.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-command=${1-"install"}
-
 rm -Rf web/bundles/tellawleadsfactory
 rm -Rf vendor/tellaw/LeadsFactoryBundle/Tellaw/LeadsFactoryBundle
 
-composer $command
+composer "$@"
 
 rm -Rf web/bundles/tellawleadsfactory
 ln -s ../../../leadsfactory/Resources/public web/bundles/tellawleadsfactory
