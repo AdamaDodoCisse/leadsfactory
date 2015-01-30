8f58d65aa92585222ee2a9f53036c5e204c91aaa
olombard <olombard@weka.fr>
2015-01-30 15:30:49 +0100

    Add leadsfactory:extract-field command

---
+++ Command/ExtractFieldCommand.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Created by Olivier Lombard
+ * @author olombard
+ * Date: 19/01/15
+ */
+
+namespace Tellaw\LeadsFactoryBundle\Command;
+
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Tellaw\LeadsFactoryBundle\Entity\Leads;
+use Tellaw\LeadsFactoryBundle\Entity\LeadsRepository;
+
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Question\Question;
+
+class ExtractFieldCommand extends Command
+{
+	/** @var LeadsRepository */
+	private $leadsRepository;
+
+	public function __construct(LeadsRepository $leadsRepository)
+	{
+		$this->leadsRepository = $leadsRepository;
+
+		parent::__construct();
+	}
+
+	protected function configure()
+	{
+		$this->setName('leadsfactory:extract-field')
+		     ->setDescription('Extract a field to its own column')
+		;
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		/** @var QuestionHelper $question_helper */
+		$question_helper = $this->getHelper('question');
+
+		$field_name = $question_helper->ask($input, $output,
+			new Question("Quel champ voulez-vous extraire ?\n(Les leads ne contenant pas ce champ ne seront pas modifiées) : ")
+		);
+
+		$attribute_name = $question_helper->ask($input, $output,
+			new Question("Vers quel attribut de l'entité ? (Celui-ci doit déjà exister) : ")
+		);
+
+		$setter = 'set'.ucfirst($attribute_name);
+		if ( ! method_exists('Tellaw\LeadsFactoryBundle\Entity\Leads', $setter)) {
+			$output->writeln('La méthode Tellaw\LeadsFactoryBundle\Entity\Leads::'.$setter.' n\'existe pas!');
+			return;
+		}
+
+		$leads = $this->leadsRepository->findAll();
+
+		$progress = new ProgressBar($output, count($leads));
+		$progress->setRedrawFrequency(50);
+
+		$count = 0;
+		/** @var Leads $lead */
+		foreach ($leads as $lead) {
+			$progress->advance();
+			$data = $lead->getData();
+			if (empty($data)) {
+				continue;
+			}
+			try {
+				$data = json_decode($data, true);
+			} catch (Exception $e) {
+				$output->writeln($e->getMessage());
+				continue;
+			}
+
+			if (array_key_exists($field_name, $data)) {
+				++$count;
+				$lead->{$setter}($data[$field_name]);
+			}
+		}
+		$progress->finish();
+		$output->writeln('');
+		$output->writeln("$count $attribute_name extraits");
+	}
+}
