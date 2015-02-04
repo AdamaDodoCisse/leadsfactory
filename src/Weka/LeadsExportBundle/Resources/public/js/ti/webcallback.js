1fb1602b71d60e7405885fb4f6052452a66e8b78
nicolas <nicrobelin@gmail.com>
2015-02-04 11:25:25 +0100

    JS dans ressources/public

--- src/Weka/LeadsExportBundle/Resources/public/js/ti/webcallback.js
+++ src/Weka/LeadsExportBundle/Resources/public/js/ti/webcallback.js
@@ -5,26 +5,37 @@ var webcallback = {
     phoneUtil: i18n.phonenumbers.PhoneNumberUtil.getInstance(),
     PNF: i18n.phonenumbers.PhoneNumberFormat,
     countryCode: '',
-    validationCodeIsCorrect: 0,
+    validationCodeIsCorrect: false,
+    interceptSubmit: true,
+    callEnabled: true,
+    callCounter: 0,
+    maxCalls: 3,
     init: function(){
-        jQuery('#callback-form').submit(function(e){ e.preventDefault();
+        jQuery('#callback-form').submit(function(e){
+
+            if(!$(this).validationEngine('validate')){
+                e.preventDefault();
+                return;
+            }
+
+            if(webcallback.interceptSubmit){
+                e.preventDefault();
+            };
 
             switch(webcallback.step){
-                case 'init':
-                    break;
                 case 'call':
                     webcallback.call();
                     break;
                 case 'check':
                     webcallback.check();
                     break;
-                case 'send':
-                    jQuery('#callback-form').submit();
+                default:
                     break;
             }
         });
         jQuery('#lffield\\[pays\\]').change(function(){
-            if(jQuery.inArray(jQuery(this).val(), ['FR', 'BE', 'LU', 'CH', 'MC']) !== -1){
+            webcallback.countryCode = jQuery(this).val();
+            if(jQuery.inArray(webcallback.countryCode, ['FR', 'BE', 'LU', 'CH', 'MC']) !== -1){
                 jQuery('#callback-step2').show();
                 webcallback.step = 'call';
             }else{
@@ -34,19 +45,31 @@ var webcallback = {
         });
     },
     call: function(){
+        if(!this.isCallEnabled()){
+            jQuery('#callback-step2').hide();
+            jQuery('#callback-step3').hide();
+            return;
+        }
         jQuery('#callback-step3').show();
         window.setTimeout(function () {
             jQuery('#webcallback-calling').hide();
             jQuery('#webcallback-new-call').show();
         }, 15000);
+        var number = this.phoneUtil.parseAndKeepRawInput(jQuery('#lffield\\[phone\\]').val(), this.countryCode);
+        this.formattedNumber = this.phoneUtil.format(number, this.PNF.E164);
         jQuery.ajax({
             url: baseUrl+'/web/twilio/call',
-            data: {phone: jQuery('#lffield\\[phone\\]').val()},
-            success: function(response){alert(response)}
+            data: {phone: this.formattedNumber},
+            success: function(response){
+                //alert(response)
+            }
         })
         this.step = 'check';
+
     },
     newCall: function(){
+        jQuery('#webcallback-calling').show();
+        jQuery('#webcallback-new-call').hide();
         this.call();
     },
     check: function(){
@@ -54,19 +77,26 @@ var webcallback = {
         jQuery.ajax({
             url: baseUrl+'/web/twilio/validate',
             data: {code: code},
-            success: function(response){
-                webcallback.validationCodeIsCorrect = response.valid;
-                alert(response.valid);
-                //webcallback.post();
+            success: function(response){//alert(response.validate);
+                webcallback.validationCodeIsCorrect = response.validate;
+                if(webcallback.validationCodeIsCorrect){
+                    webcallback.interceptSubmit = false;
+                    webcallback.step = 'post';
+                    webcallback.post();
+                }else{
+                    jQuery('#lffield\\[twilio_validation\\]').validationEngine('showPrompt', "Ce code n'est pas valide");
+                }
             },
             dataType: 'json'
         });
     },
     post: function(){
-
+        jQuery('#callback-form').submit();
+    },
+    isCallEnabled: function(){
+        this.callCounter++;
+        return this.callCounter < this.maxCalls ? true : false;
     }
-
-
 };
 
 jQuery(document).ready(function($){
