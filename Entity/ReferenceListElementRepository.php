4be8b94c29fb137c5e106335683d4ffe7d47b965
nicolas <nicrobelin@gmail.com>
2015-02-20 15:48:25 +0100

    Obternir le libellé d'une liste d'options depuis sa valeur

--- Entity/ReferenceListElementRepository.php
+++ Entity/ReferenceListElementRepository.php
@@ -14,16 +14,21 @@ class ReferenceListElementRepository extends EntityRepository
 	/**
 	 * Retourne le libellé correspondant à la valeur d'une option
 	 *
-	 * @param $listId
-	 * @param $value
+	 * @param string $listCode
+	 * @param string $value
 	 *
 	 * @return string
 	 */
-	public function getLabel($listId, $value)
+	public function getLabel($listCode, $value)
 	{
-		$listElement = $this->findOneBy(array ('referencelist_id' => $listId, 'value' => $value));
+		$sql = "SELECT e.name FROM ReferenceListElement e LEFT JOIN ReferenceList l ON e.referencelist_id=l.id WHERE l.code=:code AND e.value=:value";
+		$query = $this->_em->getConnection()->prepare($sql);
+		$query->bindValue('code', $listCode);
+		$query->bindValue('value', $value);
+		$query->execute();
+		$label = $query->fetchColumn();
 
-		return $listElement->getName();
+		return $label;
 	}
 
 }
