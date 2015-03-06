7ca2f1701be7232fcd62a240be5dd6dc35856a6f
Eric Wallet <eric.wallet@yahoo.fr>
2015-03-06 13:53:49 +0100

    update

--- Entity/ExportRepository.php
+++ Entity/ExportRepository.php
@@ -36,6 +36,7 @@ class ExportRepository extends EntityRepository
             $dql .= $where;
         }
 
+        $dql .= " ORDER BY e.created_at DESC";
 
         $query = $this->getEntityManager()
             ->createQuery($dql)
