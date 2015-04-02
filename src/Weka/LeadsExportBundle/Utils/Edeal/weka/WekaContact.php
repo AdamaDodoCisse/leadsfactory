ed9241081d9d03ce0343e23867be5a9651722a06
olombard <olombard@weka.fr>
2015-04-02 16:47:16 +0200

    Mapping E-Deal Weka "Contactez-nous"

---
+++ src/Weka/LeadsExportBundle/Utils/Edeal/weka/WekaContact.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by Olivier Lombard
+ * @author olombard
+ * Date: 02/04/15
+ */
+
+namespace Weka\LeadsExportBundle\Utils\Edeal\weka;
+
+use Weka\LeadsExportBundle\Utils\Edeal\WekaMapping;
+
+
+class WekaContact extends WekaMapping
+{
+    public function getCpwCorpName($data)
+    {
+        if(isset($data['type-etablissement']))
+            return $this->getTypeEtablissement($data['type-etablissement']) . ' - ' . $data['zip'];
+        return 'undefined';
+    }
+
+    public function getCpwComment($data)
+    {
+        $comment = 'Provient du formulaire Essai Weka';
+
+        if(!empty($data['product_name']))
+            $comment .= "\nNom produit : " . $data['product_name'];
+
+        if(!empty($data['product_sku']))
+            $comment .= "\nSKU : " . $data['product_sku'];
+
+        return $comment;
+    }
+
+    public function getCpwOriIDCode($data)
+    {
+        return 'ESSAI';
+    }
+
+    public function getCpwActIDCode($data)
+    {
+        return '';
+    }
+
+    public function getCpwTypeDemande_($data)
+    {
+        return 'Essai';
+    }
+
+    public function getCpwSku_($data)
+    {
+        return $data['product_sku'];
+    }
+
+    public function getCpwProductTitle_($data)
+    {
+        return $data['product_name'];
+    }
+
+    public function getEntCorpName($data)
+    {
+        if(isset($data['type-etablissement']))
+            return $this->getTypeEtablissement($data['type-etablissement']) . ' - ' . $data['zip'];
+        return 'undefined';
+    }
+
+    public function getCpwProfilCode($data)
+    {
+        return $data['profil'];
+    }
+}
