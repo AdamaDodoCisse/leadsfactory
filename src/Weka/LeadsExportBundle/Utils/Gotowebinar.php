96427dbb6bcdf66af17606bdf85f96b59f9609fa
nicolas <nicrobelin@gmail.com>
2015-06-17 18:10:07 +0200

    Export Gotowebinar

---
+++ src/Weka/LeadsExportBundle/Utils/Gotowebinar.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Weka\LeadsExportBundle\Utils;
+
+use Tellaw\LeadsFactoryBundle\Utils\Export\AbstractMethod;
+use Tellaw\LeadsFactoryBundle\Entity\Form;
+use Tellaw\LeadsFactoryBundle\Entity\Export;
+use Citrix\Authentication\Direct;
+
+
+class Gotowebinar extends AbstractMethod{
+
+	/** @var  \Weka\LeadsExportBundle\Utils\Gotowebinar\BaseMapping */
+    private $_mappingClass;
+
+	private $_credentials;
+
+	public function __construct($credentials)
+    {
+        $this->_credentials = $credentials;
+    }
+
+    /**
+     * Process export
+     *
+     * @param array $jobs
+     * @param Form $form
+     */
+    public function export($jobs, $form)
+    {
+        $exportUtils = $this->getContainer()->get('export_utils');
+        $logger = $this->getContainer()->get('export.logger');
+
+        $logger->info('GotoWebinar export start '.$form->getName());
+
+        $this->_mappingClass = $this->_getMapping($form);
+	    $scope = $form->getScope()->getCode();
+
+        if(is_null($this->_mappingClass)){
+            $error = 'Mapping inexistant FORM '.$form->getCode();
+            $logger->error($error);
+        }
+
+	    $client = new Direct($this->_credentials[$scope]['consumer_key']);
+	    $client->auth($this->_credentials[$scope]['user'], $this->_credentials[$scope]['password']);
+	    $goToWebinar = new \Citrix\GoToWebinar($client);
+
+	    /** @var Export $job */
+	    foreach($jobs as $job){
+
+            if(!empty($error)){
+                $job->setLog($error);
+                $job->setStatus($exportUtils->getErrorStatus($job));
+
+                $em = $this->getContainer()->get('doctrine')->getManager();
+                $em->persist($job);
+                $em->flush();
+
+                continue;
+            }
+
+	        $logger->info('job ID : '.$job->getId());
+
+            $data = json_decode($job->getLead()->getData(), true);
+		    $webinarKey = $data['gotowebinar_key'];
+
+		    $registrantData = $this->getMappedData($data, $this->_mappingClass->getMapping());
+			$registration = $goToWebinar->register($webinarKey, $registrantData);
+
+			if(!$registration->hasErrors()){
+				$log = "Exporté avec succès";
+				$status = $exportUtils::$_EXPORT_SUCCESS;
+			}else{
+				$error = $registration->getErrors();
+				$log = $error[0];
+				$status = $exportUtils->getErrorStatus($job);
+			}
+
+            $exportUtils->updateJob($job, $status, $log);
+            $exportUtils->updateLead($job->getLead(), $status, $log);
+            $logger->info($log);
+        }
+    }
+
+	private function getMappedData($data, $mapping)
+	{
+		$entity = array();
+		foreach($mapping as $gtwKey => $formKey) {
+
+			$getter = 'get'.ucfirst(strtolower($gtwKey));
+
+			if (method_exists($this->_mappingClass, $getter)){
+				$entity[$gtwKey] = $this->_mappingClass->$getter($data);
+			}elseif(!empty($formKey)){
+				$entity[$gtwKey] = isset($data[$formKey]) ? $data[$formKey] : null;
+			}else{
+				$entity[$gtwKey] = null;
+			}
+		}
+		return $entity;
+	}
+
+    /**
+     * @param $data
+     * @return \StdClass
+     *
+     */
+    private function _getCouponsWeb($data)
+    {
+	    return $this->getMappedData($data, $this->_mappingClass->getCouponsWebMapping());
+    }
+
+    /**
+     * get Mapping class
+     *
+     * @param Form $form
+     * @return mixed
+     */
+    private function _getMapping($form)
+    {
+	    $logger = $this->getContainer()->get('export.logger');
+
+	    $config = $form->getConfig();
+	    $scope = !is_null($form->getScope()) ? $form->getScope()->getCode() : null;
+	    $scopePath = !is_null($scope) ? $scope."\\" : '';
+
+	    if(isset($config['export']['gotowebinar']['mapping_class'])){
+		    $logger->info($config['export']['gotowebinar']['mapping_class']);
+		    $className = "\\Weka\\LeadsExportBundle\\Utils\\Gotowebinar\\" . $scopePath . $config['export']['edeal']['mapping_class'];
+	    }
+		$em = $this->getContainer()->get('doctrine')->getManager();
+        $list_element_repository = $this->getContainer()->get('leadsfactory.reference_list_element_repository');
+        return (class_exists($className)) ? new $className($em, $list_element_repository) : null;
+    }
+}
---
+++ src/Weka/LeadsExportBundle/Utils/Gotowebinar/weka/WekaCommerciauxWebconf.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Weka\LeadsExportBundle\Utils\Gotowebinar\weka;
+
+use Weka\LeadsExportBundle\Utils\Gotowebinar\BaseMapping;
+
+
+class WekaCommerciauxWebconf extends BaseMapping{
+
+	public function getCity($data)
+	{
+		$city = $this->list_element_repository->getNameUsingListCode('ville', $data['ville_id']);
+		return $city;
+	}
+
+	public function getOrganization($data)
+	{
+		$label = $this->list_element_repository->getNameUsingListCode('type_etablissement', $data['type-etablissement']);
+		return $label;
+	}
+
+	public function getJobTitle($data)
+	{
+		$label = $this->list_element_repository->getNameUsingListCode('fonction', $data['fonction']);
+		return $label;
+	}
+}
