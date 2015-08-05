2fada6b01e612f03128048634c7d425baaf64e17
olivier-weka <olombard@weka.fr>
2015-08-05 11:30:05 +0200

    Ajout mapping Weka Intégral (Eric)

---
+++ src/Weka/LeadsExportBundle/Utils/Edeal/weka/WekaIntegral.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Weka\LeadsExportBundle\Utils\Edeal\weka;
+
+use Weka\LeadsExportBundle\Utils\Edeal\WekaMapping;
+
+
+class WekaIntegral extends WekaMapping
+{
+    public function getCpwCorpName($data)
+    {
+        if(isset($data['type-etablissement']))
+            return $this->getTypeEtablissement($data['type-etablissement']) . ' - ' . $data['zip'];
+        return 'undefined';
+    }
+
+    public function getCpwComment($data)
+    {
+        $comment = 'Provient du formulaire Essai Weka';
+
+        if(!empty($data['product_name']))
+            $comment .= "\nNom produit : " . $data['product_name'];
+
+        if(!empty($data['product_sku']))
+            $comment .= "\nSKU : " . $data['product_sku'];
+
+        return $comment;
+    }
+
+    public function getCpwOriIDCode($data)
+    {
+        return 'WEKA_INTEGRAL';
+    }
+
+    public function getCpwActIDCode($data)
+    {
+        return '';
+    }
+
+    public function getCpwTypeDemande_($data)
+    {
+        return 'WEKA_INTEGRAL';
+    }
+
+    public function getCpwSku_($data)
+    {
+        return $data['product_sku'];
+    }
+
+    public function getCpwProductTitle_($data)
+    {
+        return $data['product_name'];
+    }
+
+    public function getEntCorpName($data)
+    {
+        if(isset($data['type-etablissement']))
+            return $this->getTypeEtablissement($data['type-etablissement']) . ' - ' . $data['zip'];
+        return 'undefined';
+    }
+}
