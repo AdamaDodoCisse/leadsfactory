03ad75ea5f9cb5e57aa4189d25f6d8ef486c9063
Eric Wallet <eric.wallet@yahoo.fr>
2015-08-31 07:45:15 +0200

    update methods to access lists

--- Entity/ReferenceListElementRepository.php
+++ Entity/ReferenceListElementRepository.php
@@ -1,5 +1,5 @@
 <?php
-
+// weka-leadsfactory-master\vendor\tellaw\leadsfactory\Tellaw\LeadsFactoryBundle\Entity\ReferenceListElementRepository.php
 namespace Tellaw\LeadsFactoryBundle\Entity;
 
 use Doctrine\ORM\EntityRepository;
@@ -9,47 +9,97 @@ use Psr\Log\LoggerInterface;
  * ReferenceListElementRepository
  *
  */
-class ReferenceListElementRepository extends EntityRepository
-{
+class ReferenceListElementRepository extends EntityRepository {
+
     /** @var LoggerInterface */
     private $logger;
 
-    public function setLogger(LoggerInterface $logger)
-    {
+    public function setLogger(LoggerInterface $logger) {
         $this->logger = $logger;
     }
 
-	/**
-	 * Retourne le libellé correspondant à la valeur d'une option
-	 *
-	 * @param string $list_code
-	 * @param string $element_value
-	 *
-	 * @return string
-	 */
-	public function getNameUsingListCode($list_code, $element_value)
-	{
+    /**
+     * Retourne le libellé correspondant à la valeur d'une option
+     *
+     * @param string $list_code
+     * @param string $element_value
+     *
+     * @return string
+     */
+    
+    public function getNameUsingListCode($list_code, $element_value) {
         if (empty($element_value)) {
             return '';
         }
-		$qb = $this->getEntityManager()->createQueryBuilder();
-		$qb->select('e.name')
-		    ->from('TellawLeadsFactoryBundle:ReferenceListElement', 'e')
-		    ->join('TellawLeadsFactoryBundle:ReferenceList', 'l')
-		    ->where('l.code = :code')
-			->andWhere('e.referencelist_id = l.id')
-		    ->andWhere('e.value = :value')
-		    ->setParameter('code', $list_code)
-		    ->setParameter('value', $element_value)
-		;
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb->select('e.name')
+                ->from('TellawLeadsFactoryBundle:ReferenceListElement', 'e')
+                ->join('TellawLeadsFactoryBundle:ReferenceList', 'l')
+                ->where('l.code = :code')
+                ->andWhere('e.referencelist_id = l.value')
+                ->andWhere('e.value = :value')
+                ->setParameter('code', $list_code)
+                ->setParameter('value', $element_value)
+        ;
         $query = $qb->getQuery();
 
         try {
             $result = $query->getSingleScalarResult();
-        } catch(\Exception $e) {
-            //$this->logger->warning($e->getMessage());
+        } catch (\Exception $e) {
             $result = '';
         }
         return $result;
-	}
+    }
+    
+    // get nom ville :      list_code   
+    public function getNameUsingListCodeAndValue($list_code, $element_value) {
+        if (empty($element_value)) {
+            return '';
+        }
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb->select('e.name')
+                ->from('TellawLeadsFactoryBundle:ReferenceListElement', 'e')
+                ->join('e.referenceList', 'l')
+                ->where('l.code = :code')
+                ->andWhere('e.value = :value')
+                ->setParameter('code', $list_code)
+                ->setParameter('value', $element_value)
+        ;
+        
+        $query = $qb->getQuery();
+        
+        try {
+            $result = $query->getSingleScalarResult();
+        } catch (\Exception $e) {
+            $result = '';
+        }
+        
+        return $result;
+    }
+    
+    
+    //      ("nbhabitants", $data['ville'])
+    public function getValueUsingListCodeAndName($list_code, $element_value) {
+        if (empty($element_value)) {
+            return '';
+        }
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb->select('e.value')
+                ->from('TellawLeadsFactoryBundle:ReferenceListElement', 'e')
+                ->join('e.referenceList', 'l')
+                ->where('l.code = :code')
+                ->andWhere('e.name = :name')
+                ->setParameter('code', $list_code)
+                ->setParameter('name', $element_value)
+        ;
+        
+        $query = $qb->getQuery();
+        try {
+            $result = $query->getSingleScalarResult();
+        } catch (\Exception $e) {
+            $result = '';
+        }        
+        return $result;
+    }
+
 }
