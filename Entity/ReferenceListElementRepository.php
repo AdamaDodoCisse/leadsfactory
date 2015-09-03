0503f5b9868cce570ffde3d5c5d4df4eb393991c
Eric Wallet <eric.wallet@yahoo.fr>
2015-09-03 15:35:00 +0200

    Fix element repository

--- Entity/ReferenceListElementRepository.php
+++ Entity/ReferenceListElementRepository.php
@@ -26,63 +26,58 @@ class ReferenceListElementRepository extends EntityRepository {
      *
      * @return string
      */
-    
-    public function getNameUsingListCode($list_code, $element_value) {
+
+    public function getNameUsingListCode($list_code, $element_value, $return_type = 0)
+    {
         if (empty($element_value)) {
             return '';
         }
         $qb = $this->getEntityManager()->createQueryBuilder();
         $qb->select('e.name')
-                ->from('TellawLeadsFactoryBundle:ReferenceListElement', 'e')
-                ->join('TellawLeadsFactoryBundle:ReferenceList', 'l')
-                ->where('l.code = :code')
-                ->andWhere('e.referencelist_id = l.value')
-                ->andWhere('e.value = :value')
-                ->setParameter('code', $list_code)
-                ->setParameter('value', $element_value)
+            ->from('TellawLeadsFactoryBundle:ReferenceListElement', 'e')
+            ->join('TellawLeadsFactoryBundle:ReferenceList', 'l')
+            ->where('l.code = :code')
+            ->andWhere('e.referencelist_id = l.id')
+            ->andWhere('e.value = :value')
+            ->setParameter('code', $list_code)
+            ->setParameter('value', $element_value)
         ;
         $query = $qb->getQuery();
 
         try {
-            $result = $query->getSingleScalarResult();
-        } catch (\Exception $e) {
+            if (!$return_type) {
+                $result = $query->getSingleScalarResult();
+            } else{
+                $result = $query->getResult();
+            }
+        } catch(\Exception $e) {
+            //$this->logger->warning($e->getMessage());
             $result = '';
         }
         return $result;
     }
-    
+
+    /**
+     * @deprecated
+     *
+     * @param $list_code
+     * @param $element_value
+     * @return string
+     */
     public function getNameUsingListCodeAndValue($list_code, $element_value) {
-        if (empty($element_value)) {
-            return '';
-        }
-        $qb = $this->getEntityManager()->createQueryBuilder();
-        $qb->select('e.name')
-                ->from('TellawLeadsFactoryBundle:ReferenceListElement', 'e')
-                ->join('e.referenceList', 'l')
-                ->where('l.code = :code')
-                ->andWhere('e.value = :value')
-                ->setParameter('code', $list_code)
-                ->setParameter('value', $element_value)
-        ;
-        
-        $query = $qb->getQuery();
-        
-        try {
-            $result = $query->getResult();            
-        } catch (\Exception $e) {
-            $result = '';
-        }
-        return $result;
+        return $this->getNameUsingListCode($list_code, $element_value, 1);
     }
     
     public function getValueUsingListCodeAndName($list_code, $element_value) {
         if (empty($element_value)) {
             return '';
         }
+
         $qb = $this->getEntityManager()->createQueryBuilder();
         $qb->select('e.value')
                 ->from('TellawLeadsFactoryBundle:ReferenceListElement', 'e')
                 ->join('e.referenceList', 'l')
+                ->andWhere('e.referencelist_id = l.id')
                 ->where('l.code = :code')
                 ->andWhere('e.name = :name')
                 ->setParameter('code', $list_code)
