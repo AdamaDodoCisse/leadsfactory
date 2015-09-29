7555e07c8986bce171b39da6e1dcf9bf3be2f100
nicolas <nicrobelin@gmail.com>
2015-09-29 12:07:57 +0200

    FIX export gotowebinar

--- src/Weka/LeadsExportBundle/Utils/Gotowebinar/weka/WekaCommerciauxWebconf.php
+++ src/Weka/LeadsExportBundle/Utils/Gotowebinar/weka/WekaCommerciauxWebconf.php
@@ -10,13 +10,13 @@ class WekaCommerciauxWebconf extends BaseMapping{
 	public function getCity($data)
 	{
 		$city = $this->list_element_repository->getNameUsingListCode('ville', $data['ville_id']);
-		return $city;
+		return !empty($city) ? $city : 'inconnu' ;
 	}
 
 	public function getOrganization($data)
 	{
 		$label = $this->list_element_repository->getNameUsingListCode('type_etablissement', $data['type-etablissement']);
-		return $label;
+		return !empty($label) ? $label : 'inconnu' ;
 	}
 
 	public function getJobTitle($data)
