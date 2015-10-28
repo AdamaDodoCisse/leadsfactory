eef8eea98d8be572fb87b94f7098e9118b50e39c
Eric Wallet <eric.wallet@yahoo.fr>
2015-10-28 10:45:55 +0100

    Fixes #6 - Verrou lors de l'export

--- Command/ExportCommand.php
+++ Command/ExportCommand.php
@@ -10,7 +10,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class ExportCommand extends ContainerAwareCommand {
 	
-
+    private static $_PID_FILE = "export_command_is_running.lock";
 	
 	protected function configure() {
 		$this->setName('leadsfactory:export:leads')
@@ -22,31 +22,75 @@ class ExportCommand extends ContainerAwareCommand {
     protected function execute(InputInterface $input, OutputInterface $output) {
 
         $logger = $this->getContainer()->get('export.logger');
-        $doctrine = $this->getContainer()->get('doctrine');
 
-        $form = $input->getArgument('form');
-        if($form){
-            $output->writeln('Exporting '.$form.' leads...');
-            $forms = $this->getContainer()->get('leadsfactory.form_repository')->findByCode($form);
-        }else{
-            $output->writeln('Exporting all leads...');
-            $forms = $this->getContainer()->get('leadsfactory.form_repository')->findAll();
-        }
+        if ( !$this->isExportRunning() ) {
 
-        foreach($forms as $form){
-            $output->writeln($form->getName());
-            try{
-                $this->getContainer()->get('export_utils')->export($form);
-                $output->writeln('Done');
-            }catch(\Exception $e){
-                $output->writeln('Error : '.$e->getMessage());
-	            $output->writeln('Error : '.$e->getTraceAsString());
-	            $logger->error('Export '.$form->getName(). ' error : '.$e->getMessage());
-	            $logger->error($e->getTraceAsString());
+            $this->createExportPid();
+            $output->writeln('Info : Created LOCK file : ' . ExportCommand::$_PID_FILE);
+
+            $form = $input->getArgument('form');
+            if ($form) {
+                $output->writeln('Exporting ' . $form . ' leads...');
+                $forms = $this->getContainer()->get('leadsfactory.form_repository')->findByCode($form);
+            } else {
+                $output->writeln('Exporting all leads...');
+                $forms = $this->getContainer()->get('leadsfactory.form_repository')->findAll();
             }
+
+            foreach ($forms as $form) {
+                $output->writeln($form->getName());
+                try {
+                    $this->getContainer()->get('export_utils')->export($form);
+                    $output->writeln('Done');
+                } catch (\Exception $e) {
+                    $output->writeln('Error : ' . $e->getMessage());
+                    $output->writeln('Error : ' . $e->getTraceAsString());
+                    $logger->error('Export ' . $form->getName() . ' error : ' . $e->getMessage());
+                    $logger->error($e->getTraceAsString());
+                }
+            }
+
+            $this->removePidFile();
+            $output->writeln('Info : Removed LOCK file : ' . ExportCommand::$_PID_FILE);
+
+        } else {
+            $output->writeln('Un export est déjà en traitement. Si c\'est anormal, merci de détruire le fichier LOCK :'.ExportCommand::$_PID_FILE );
+            $logger->error('Un export est déjà en traitement. Si c\'est anormal, merci de détruire le fichier LOCK :'.ExportCommand::$_PID_FILE);
         }
 	}
 
+    private function createExportPid () {
 
+        $somecontent = date("c");
+
+
+        if (!$handle = fopen(ExportCommand::$_PID_FILE, 'a')) {
+            throw new \Exception ("Impossible d'ouvrir le fichier (".ExportCommand::$_PID_FILE.")");
+            exit;
+        }
+
+        if (fwrite($handle, $somecontent) === FALSE) {
+            throw new \Exception ("Impossible d'écrire dans le fichier (".ExportCommand::$_PID_FILE.")");
+            exit;
+        }
+
+        fclose($handle);
+
+    }
+
+    private function isExportRunning () {
+        if (file_exists( ExportCommand::$_PID_FILE )) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private function removePidFile () {
+        unlink (ExportCommand::$_PID_FILE);
+        if (file_exists( ExportCommand::$_PID_FILE )) {
+            throw new \Exception ("Impossible de retirer le fichier LOCK (".ExportCommand::$_PID_FILE.")");
+        }
+    }
 
 }
---
+++ Command/FonctionnalTesting.php
@@ -0,0 +1,40 @@
+<?php 
+
+namespace Tellaw\LeadsFactoryBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class FonctionnalTestingCommand extends ContainerAwareCommand {
+	
+	private $cronjobs = array();
+	
+	protected function configure() {
+		$this
+		->setName('leadsfactory:testing:run')
+		->setDescription('Command running foncitonnal testing of forms.')
+		;
+	}
+
+    /**
+     *
+     * execute
+     *
+     * Method used to set yesterdays status history in database.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output) {
+
+        // Find every forms
+        $client = static::createClient();
+
+	}
+
+
+}
