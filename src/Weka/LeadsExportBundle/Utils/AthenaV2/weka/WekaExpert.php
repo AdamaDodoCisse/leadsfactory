984910dba1188e57d1f52c42118ecf708aba3803
nrobelin <nicrobelin@gmail.com>
2016-01-11 13:53:02 +0100

    ST-5421 : Export Athena wk_expert

---
+++ src/Weka/LeadsExportBundle/Utils/AthenaV2/weka/WekaExpert.php
@@ -0,0 +1,11 @@
+<?php
+namespace Weka\LeadsExportBundle\Utils\AthenaV2\weka;
+
+use Weka\LeadsExportBundle\Utils\AthenaV2\AthenaV2BaseMapping;
+
+class WekaExpert extends AthenaV2BaseMapping {
+    
+    /*public function getType_demande ( $data ) {
+        return "wk_integral";
+    }*/
+}
