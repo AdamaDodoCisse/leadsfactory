0937ae87eded8fc22b6c7f7d506dcb5b547518ee
nrobelin <nicrobelin@gmail.com>
2016-05-11 18:00:55 +0200

    Magallanes : Debug tâche post déploiement "Hack vendors"

--- .mage/tasks/HackVendor.php
+++ .mage/tasks/HackVendor.php
@@ -17,7 +17,7 @@ class HackVendor extends AbstractTask
         //return $this->runCommand('sed -i "/"curlopts" => array(/ a\\CURLOPT_SSL_VERIFYHOST => 0, CURLOPT_SSL_VERIFYPEER => 0 // hack" vendor/twilio/sdk/Services/Twilio.php');
 
         $result = $this->runCommandLocal('rm -rf vendor/twilio');
-        $result = $this->runCommandLocal('ln -s vendor/twilio /data/apps/leads-factory/shared/twilio');
+        $result = $this->runCommandLocal('ln -s /data/apps/leads-factory/shared/twilio /data/apps/leads-factory/current/vendor/twilio');
 
         return true;
     }
