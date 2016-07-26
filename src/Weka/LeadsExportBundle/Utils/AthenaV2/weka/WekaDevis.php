e77c869291be41d91b7b126db2516f6d91e27e8e
nrobelin <nicrobelin@gmail.com>
2016-07-26 16:58:21 +0200

    Connecteur WK devis

---
+++ src/Weka/LeadsExportBundle/Utils/AthenaV2/weka/WekaDevis.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Weka\LeadsExportBundle\Utils\AthenaV2\weka;
+
+use Weka\LeadsExportBundle\Utils\AthenaV2\AthenaV2BaseMapping;
+
+class WekaDevis extends AthenaV2BaseMapping {
+    
+}
