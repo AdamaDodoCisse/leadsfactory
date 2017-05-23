ad27b6784c076918ac82ce3a5469a2130d89fb0c
seth AMG <samegavie@webnet.fr>
2017-05-23 11:06:08 +0200

    corrections failed merge

--- src/Weka/LeadsExportBundle/Utils/AthenaV2/ti/TIService.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Weka\LeadsExportBundle\Utils\AthenaV2\ti;
-
-use Weka\LeadsExportBundle\Utils\AthenaV2\AthenaV2BaseMapping;
-
-class TIService extends AthenaV2BaseMapping {
-
-}
--- src/Weka/LeadsExportBundle/WekaLeadsExportBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Weka\LeadsExportBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class WekaLeadsExportBundle extends Bundle
-{
-}
